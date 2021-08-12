44a73cc74456c88a0eb8a9a83014ad3886dc35ef
pysolver33 <pysolver33@gmail.com>
2021-08-12 22:02:58 +0800

    basic google maps app

---
+++ maps/src/index.ts
@@ -0,0 +1,9 @@
+import {User,red} from './User';
+import {Company} from './Company'
+
+const api_key = 'AIzaSyBNLrJhOMz6idD05pzfn5lhA-TAw-mAZCU';
+const user = new User();
+const company = new Company();
+console.log(red);
+console.log(user)
+console.log(company)
